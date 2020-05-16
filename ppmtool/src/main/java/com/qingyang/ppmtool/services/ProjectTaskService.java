0e5cc5f5f3cb8324db854137d6b78840ba69e69c
Qingyang Liu <qingyang@Qingyangs-MacBook-Pro.local>
2020-05-15 22:00:43 -0500

    create project task

---
+++ ppmtool/src/main/java/com/qingyang/ppmtool/services/ProjectTaskService.java
@@ -0,0 +1,45 @@
+package com.qingyang.ppmtool.services;
+
+import com.qingyang.ppmtool.domain.Backlog;
+import com.qingyang.ppmtool.domain.Project;
+import com.qingyang.ppmtool.domain.ProjectTask;
+import com.qingyang.ppmtool.repositories.BacklogRepository;
+import com.qingyang.ppmtool.repositories.ProjectTaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectTaskService {
+
+    @Autowired
+    private BacklogRepository backlogRepository;
+
+    @Autowired
+    private ProjectTaskRepository projectTaskRepository;
+
+    public ProjectTask addProjectTask(String projectIdentifier, ProjectTask projectTask) {
+        // TODO: EXCEPTION: Project not FOUND
+
+        Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
+        projectTask.setBacklog(backlog);
+
+        // We want our project sequence to be like this: IDPRO-1  IDPRO-2  ...100 101
+        Integer BacklogSequence = backlog.getPTSequence();
+        BacklogSequence ++;
+        backlog.setPTSequence(BacklogSequence);
+
+        // Add sequence and projectIdentifier to projectTask
+        projectTask.setProjectSequence(backlog.getProjectIdentifier() + '-' + BacklogSequence);
+        projectTask.setProjectIdentifier(projectIdentifier);
+
+        if (projectTask.getStatus() == "" || projectTask.getStatus() == null) {
+            projectTask.setStatus("TODO");
+        }
+
+        if (projectTask.getPriority() == null) {
+            projectTask.setPriority(3);
+        }
+
+        return projectTaskRepository.save(projectTask);
+    }
+}
---
+++ ppmtool/src/main/java/com/qingyang/ppmtool/web/BacklogController.java
@@ -0,0 +1,37 @@
+package com.qingyang.ppmtool.web;
+
+import com.qingyang.ppmtool.domain.ProjectTask;
+import com.qingyang.ppmtool.services.MapValidationErrorService;
+import com.qingyang.ppmtool.services.ProjectTaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api/backlog")
+@CrossOrigin
+public class BacklogController {
+
+    @Autowired
+    private ProjectTaskService projectTaskService;
+
+    @Autowired
+    private MapValidationErrorService mapValidationErrorService;
+
+    @PostMapping("/{projectIdentifier}")
+    public ResponseEntity<?> addPTtoBacklog(@Valid @RequestBody ProjectTask projectTask,
+                                            @PathVariable String projectIdentifier,
+                                            BindingResult result) {
+        ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
+        if (errorMap != null) {
+            return errorMap;
+        }
+
+        ProjectTask savedProjectTask = projectTaskService.addProjectTask(projectIdentifier, projectTask);
+        return new ResponseEntity<ProjectTask>(savedProjectTask, HttpStatus.CREATED);
+    }
+}
