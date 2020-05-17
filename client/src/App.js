ecfa0711f3df6ebeba38c09f53542f4a331e9b0d
Qingyang Liu <qingyang@Qingyangs-MacBook-Pro.local>
2020-05-16 21:37:48 -0500

    project board and create pt components

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,8 @@ import Header from './components/Layout/Header';
 import Dashboard from './components/Dashboard';
 import AddProject from './components/Project/AddProject';
 import UpdateProject from './components/Project/UpdateProject';
+import ProjectBoard from './components/ProjectBoard/ProjectBoard';
+import AddProjectTask from './components/ProjectBoard/ProjectTasks/AddProjectTask';
 import './App.css';
 
 function App() {
@@ -18,6 +20,8 @@ function App() {
 					<Route path='/dashboard' exact component={Dashboard}/>
 					<Route path='/addProject' exact component={AddProject}/>
 					<Route path='/updateProject/:id' exact component={UpdateProject}/>
+					<Route path='/projectBoard/:id' exact component={ProjectBoard}/>
+					<Route path='/addProjectTask/:id' exact component={AddProjectTask}/>
 				</div>
 			</BrowserRouter>
 		</Provider>
