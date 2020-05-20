dd5cfcdb3e78b1ddc56b620492acd3bce7217970
Qingyang Liu <qingyang@Qingyangs-MacBook-Pro.local>
2020-05-19 23:18:33 -0500

    user login generates JWT token

---
+++ ppmtool/src/main/java/com/qingyang/ppmtool/security/JwtTokenProvider.java
@@ -0,0 +1,42 @@
+package com.qingyang.ppmtool.security;
+
+import com.qingyang.ppmtool.domain.User;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class JwtTokenProvider {
+
+    /* Generate token */
+    public String generateToken(Authentication authentication) {
+        User user = (User) authentication.getPrincipal();
+        Date now = new Date(System.currentTimeMillis());
+
+        Date expiryDate = new Date(now.getTime() + SecurityConstants.EXPIRATION_TIME);
+        String userId = Long.toString(user.getId());
+
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("id", userId);
+        claims.put("username", user.getUsername());
+        claims.put("fullname", user.getFullname());
+
+        return Jwts.builder()
+                .setSubject(userId)
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(expiryDate)
+                .signWith(SignatureAlgorithm.HS512, SecurityConstants.SECRET)
+                .compact();
+    }
+
+    /* Validate token */
+
+
+    /* Get userId from token */
+}
--- ppmtool/src/main/java/com/qingyang/ppmtool/web/UserController.java
+++ ppmtool/src/main/java/com/qingyang/ppmtool/web/UserController.java
@@ -1,12 +1,20 @@
 package com.qingyang.ppmtool.web;
 
 import com.qingyang.ppmtool.domain.User;
+import com.qingyang.ppmtool.payload.JWTLoginSucessReponse;
+import com.qingyang.ppmtool.payload.LoginRequest;
+import com.qingyang.ppmtool.security.JwtTokenProvider;
+import com.qingyang.ppmtool.security.SecurityConstants;
 import com.qingyang.ppmtool.services.MapValidationErrorService;
 import com.qingyang.ppmtool.services.UserService;
 import com.qingyang.ppmtool.validator.UserValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -28,6 +36,12 @@ public class UserController {
     @Autowired
     private UserValidator userValidator;
 
+    @Autowired
+    private JwtTokenProvider jwtTokenProvider;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
     @PostMapping("/register")
     public ResponseEntity<?> register(@Valid @RequestBody User user, BindingResult result) {
         userValidator.validate(user, result);
@@ -39,5 +53,24 @@ public class UserController {
 
         User newUser = userService.saveUser(user);
         return new ResponseEntity<User>(newUser, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@Valid @RequestBody LoginRequest loginRequest, BindingResult result) {
+        ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
+        if (errorMap != null) {
+            return errorMap;
+        }
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginRequest.getUsername(),
+                        loginRequest.getPassword()
+                )
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwtToken = SecurityConstants.TOKEN_PREFIX + jwtTokenProvider.generateToken(authentication);
+        return ResponseEntity.ok(new JWTLoginSucessReponse(true, jwtToken));
     }
 }
