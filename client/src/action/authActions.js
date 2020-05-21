8f8436670084c7e6f91eaeabae348fa2b0355c89
Qingyang Liu <qingyang@Qingyangs-MacBook-Pro.local>
2020-05-21 02:31:04 -0500

    auth reducer, set authorization header upon login

--- client/src/action/authActions.js
+++ client/src/action/authActions.js
@@ -1,5 +1,8 @@
 import axios from "axios";
+import jwt_decode from 'jwt-decode';
 import { SET_CURRENT_USER, GET_ERRORS } from './types';
+import setJWTToken from '../securityUtils/setJWTToken';
+
 
 export const register = (newUser, history) => async dispatch => {
     try {
@@ -11,6 +14,28 @@ export const register = (newUser, history) => async dispatch => {
         });
     } catch(err) {
         console.log(err);
+        dispatch({
+            type: GET_ERRORS,
+            payload: err.response.data
+        })
+    }
+}
+
+
+export const login = loginCredential => async dispatch => {
+    try {
+        const res = axios.post("/api/users/login", loginCredential);
+        const { token } = res.data;
+
+        localStorage.setItem('token', token);
+        setJWTToken(token);
+
+        const decoded = jwt_decode(token);
+        dispatch({
+            type: SET_CURRENT_USER,
+            payload: decoded
+        })
+    } catch(err) {
         dispatch({
             type: GET_ERRORS,
             payload: err.response.data
